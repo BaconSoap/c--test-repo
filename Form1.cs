3fecf2098b326ac3e31da56f7bc89b87a225fb9b
Andrew Varnerin <ffac06@gmail.com>
2010-10-28 19:58:13 -0400

    Now 5% more complicated due to moving some functions to delegates.

--- Form1.cs
+++ Form1.cs
@@ -11,6 +11,17 @@ namespace git_test
 {
     public partial class Form1 : Form
     {
+
+        private delegate int say();
+
+        private void s(say s)
+        {
+            if (s() == 1)
+            {
+                Form1.ActiveForm.Text = "12313";
+            };
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +30,7 @@ namespace git_test
         private void Sayhello(object sender, EventArgs e)
         {
             notifyIcon1.ShowBalloonTip(1000);
+            s(() => 1);
         }
 
         private static void Exit(object sender, EventArgs e)
