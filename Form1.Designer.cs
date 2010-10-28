17ab89b98058b1a5748a5b44c52576606e3ef7d1
Andrew Varnerin <ffac06@gmail.com>
2010-10-28 16:48:50 -0400

    Made an exit button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,17 +32,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(109, 123);
+            this.button1.Location = new System.Drawing.Point(12, 123);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Say Hello!";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.sayhello);
+            this.button1.Click += new System.EventHandler(this.Sayhello);
             // 
             // notifyIcon1
             // 
@@ -53,11 +54,22 @@
             this.notifyIcon1.Text = "Hello!";
             this.notifyIcon1.Visible = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit :(";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(Exit);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 268);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -69,6 +81,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
